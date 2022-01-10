c95bd67f13f274ee263c7eb42e24ab64f5d0b0c9
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-01-10 10:07:31 +0530

    Added attendance tracker

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# chrome_extension
+# Meet Attendance Collector
+###### chrome extension
--- attendance.js
+++ attendance.js
@@ -1,3 +1,81 @@
+let studentslist = new Set();
+let studentData = new Map();
+let meet_duration = 0;
+let StartTime = new Date().toLocaleTimeString();
+
+function track_attendance() {
+    let currently_present_students = document.getElementsByClassName("ZjFb7c");
+    studentslist.clear();
+    for (i = 0; i < currently_present_students.length; i++) {
+        studentslist.add(currently_present_students[i].innerHTML.toUpperCase());
+    }
+
+    for (student of studentslist) {
+        if (studentData.has(student)) {
+            let data = studentData.get(student);
+            data[0] += 1;
+            studentData.set(student, data);
+        } else {
+            let join_time = new Date().toLocaleTimeString();
+            let curr_status = 1;
+            let data = [];
+            data.push(curr_status);
+            data.push(join_time);
+            studentData.set(student, data);
+        }
+    }
+
+    meet_duration += 1;
+}
+
+function start() {
+    tracking = setInterval(track_attendance, 1000);
+}
+
+let stop = STOP = function() {
+    clearInterval(tracking);
+    let mac_window = window.open('http://meetattendance.herokuapp.com/mac/save.html');
+    let meet_code = window.location.pathname.substring(1);
+    let date = new Date();
+    let dd = date.getDate();
+    let mm = date.toLocaleString('default', { month: 'short' });
+    let yyyy = date.getFullYear();
+    date = dd + '/' + mm + "/" + yyyy;
+
+    let attended_student_list = [];
+    let attended_student_duration = [];
+    let attended_student_join_time = [];
+    let data_keys = studentData.keys();
+
+    for (i = 0; i < studentData.size; i++) {
+        let student = data_keys.next().value;
+        attended_student_list.push(student);
+    }
+
+    attended_student_list.sort();
+
+    for (student of attended_student_list) {
+        let data = studentData.get(student);
+        attended_student_duration.push(data[0]);
+        attended_student_join_time.push(data[1]);
+    }
+
+    var attendance_details = {
+        meet_code: meet_code,
+        date: date,
+        start_time: StartTime,
+        stop_time: new Date().toLocaleTimeString(),
+        student_names: attended_student_list,
+        attended_duration: attended_student_duration,
+        join_time: attended_student_join_time,
+        meet_duration: meet_duration
+    }
+
+    setTimeout(function() {
+        mac_window.postMessage(JSON.stringify(attendance_details), 'http://meetattendance.herokuapp.com/mac/save.html')
+    }, 5000);
+}
+
 // Adding attendance status to meet ui 
 let macstatus = document.createElement("button");
 macstatus.id = "macstatus";
@@ -21,6 +99,7 @@ setInterval(function() {
 function add_status() {
     try {
         document.getElementsByClassName("SQHmX")[0].appendChild(macstatus);
+        start();
         clearInterval(status_text);
     } catch (error) {}
 }
