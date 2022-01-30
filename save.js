40000db1667970d597c840d859c3e4b21d8d87aa
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-01-30 14:51:26 +0530

    updated save.js

--- save.js
+++ save.js
@@ -1,4 +1,6 @@
 chrome.storage.sync.get(['latest_meet_attendance'], function(item) {
-    localStorage.setItem('latest_meet_attendance', JSON.stringify(item.latest_meet_attendance));
-    chrome.storage.sync.clear();
+    if (item.latest_meet_attendance != null && item.latest_meet_attendance != undefined && item.latest_meet_attendance != 'undefined ') {
+        localStorage.setItem('latest_meet_attendance', JSON.stringify(item.latest_meet_attendance));
+        chrome.storage.sync.clear();
+    }
 });
